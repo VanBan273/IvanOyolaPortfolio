a6fa7229da1a13426874f6ef28990e05f9a37c96
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2022-01-21 00:15:05 +0100

    a ver

--- portfolio/src/components/Form/Form.tsx
+++ portfolio/src/components/Form/Form.tsx
@@ -77,7 +77,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6LcAu-IdAAAAAJOTI5E_eRltZNQCvukIl2-f1glQ"
+          sitekey="6LfYriceAAAAAAjaKupD1woi3FB-dloUvQXXOLsF"
           onChange={(e) => {
             setIsHuman(true)
           }}
