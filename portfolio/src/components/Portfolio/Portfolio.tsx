b11b3d71f0a2390b790d85dc437b454d22e53085
VanBan273 <87871463+VanBan273@users.noreply.github.com>
2023-01-15 20:37:57 +0100

    cambio
    un cambio en el orden

--- portfolio/src/components/Portfolio/Portfolio.tsx
+++ portfolio/src/components/Portfolio/Portfolio.tsx
@@ -154,6 +154,35 @@ export function Portfolio(){
           </div>
         </ScrollAnimation>
 
+
+
+        <ScrollAnimation animateIn="flipInX">
+        <div className="project">
+          <header>
+          <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#ff8000 " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+          <div className="project-links">
+            <a href="https://github.com/VanBan273/tamagochi" target="_blank" rel="noreferrer">
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+          </div>
+          </header>
+          <div className="body">
+            <h3>Tamagochi</h3>
+            <p>Tipico tamagochi con el que puedes jugar y alimentarlo y si no lo atiendes, tras cierto tiempo, muere.
+            </p>
+          </div>
+          <footer>
+            <ul className="tech-list">
+              <li>React</li>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              
+            </ul>
+          </footer>
+        </div>
+      </ScrollAnimation>
+
+
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
@@ -183,31 +212,7 @@ export function Portfolio(){
         </ScrollAnimation>
 
 
-        <ScrollAnimation animateIn="flipInX">
-        <div className="project">
-          <header>
-          <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#ff8000 " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-          <div className="project-links">
-            <a href="https://github.com/VanBan273/tamagochi" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
-            </a>
-          </div>
-          </header>
-          <div className="body">
-            <h3>Tamagochi</h3>
-            <p>Tipico tamagochi con el que puedes jugar y alimentarlo y si no lo atiendes, tras cierto tiempo, muere.
-            </p>
-          </div>
-          <footer>
-            <ul className="tech-list">
-              <li>React</li>
-              <li>JavaScript</li>
-              <li>HTML</li>
-              
-            </ul>
-          </footer>
-        </div>
-      </ScrollAnimation>
+
 
 
 
